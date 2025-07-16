85fdbe213eb59d5534137917b533ba17ba8bc9c6
Helene Gaspar <helene.gaspar@velopass.com>
2025-07-16 19:57:37 +0200

    wasn't able to create address-autocomplete feature DO NOT MERGE THIS

---
+++ app/javascript/controllers/address_autocomplete_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+// Connects to data-controller="address-autocomplete"
+export default class extends Controller {
+  static values = { apiKey: String }
+
+  static targets = ["location_lost"]
+
+  connect() {
+    console.log("hello there!")
+    console.log(this.apiKeyValue)
+    console.log(this.targets)
+    this.geocoder = new MapboxGeocoder({
+      accessToken: this.apiKeyValue,
+      types: "country,region,place,postcode,locality,neighborhood,address"
+    })
+    this.geocoder.addTo(this.element)
+  }
+
+//   disconnect() {
+//     this.geocoder.onRemove()
+//   }
+  };
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,3 +2,8 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+import { Application } from "@hotwired/stimulus"
+import AddressAutocompleteController from "./address_autocomplete_controller"
+
+window.Stimulus = Application.start()
+Stimulus.register("address-autocomplete", AddressAutocompleteController)
